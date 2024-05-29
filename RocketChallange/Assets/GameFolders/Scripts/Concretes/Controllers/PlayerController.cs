1c6a36a284a50dd4bec174e4fba64b99cd339ec9
Mert Yiğit <meert.ygtt@gmail.com>
2024-05-29 14:30:26 +0300

    Folders

---
+++ RocketChallange/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RocketChallange3D.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
+
+
