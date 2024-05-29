9b24e037e31ccb917f32b9136b249218d19529a7
Mert Yiğit <meert.ygtt@gmail.com>
2024-05-29 15:23:47 +0300

    Player Controller

--- RocketChallange/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ RocketChallange/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,12 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RocketChallange.Inputs;
 using UnityEngine;
 
-namespace RocketChallange3D.Controllers
-{
+namespace RocketChallange.Controllers
+{ 
     public class PlayerController : MonoBehaviour
     {
-        
+        [SerializeField] private float force;
+        private Rigidbody _rigidbody;
+        private DefaultInput _input;
+
+        private bool _isForceUp;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _input = new DefaultInput();
+        }
+
+        private void Update()
+        {
+            if (_input.IsForceUp)
+            {
+                _isForceUp = true;
+            }
+            else
+            {
+                _isForceUp = false;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isForceUp)
+            {
+                _rigidbody.AddForce(Vector3.up * force, ForceMode.Force);
+            }
+        }
     }
 }
 
---
+++ RocketChallange/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RocketChallange.Inputs
+{ 
+    public class DefaultInput
+    {
+        private DefaultAction _input;
+        
+        public bool IsForceUp { get; private set; }
+
+        public DefaultInput()
+        {
+            _input = new DefaultAction();
+
+            _input.Rocket.ForceUp.performed += context => IsForceUp = context.ReadValueAsButton();
+            
+            _input.Enable();
+        }
+    }
+}
+
